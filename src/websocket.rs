3aefafdda1fdc9a34dadbb7ffae27b29b58c22c0
YUCLing <luotianyi@luotianyi.me>
2025-02-10 00:04:42 +0800

    improve: websocket implementation

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,5 +1,3 @@
-use std::{sync::Arc, time::Duration};
-
 use axum::{
     extract::{
         ws::{Message, WebSocket, WebSocketUpgrade},
@@ -7,10 +5,7 @@ use axum::{
     },
     response::IntoResponse,
 };
-use tokio::{
-    runtime::Handle,
-    sync::{watch::Receiver, Mutex},
-};
+use tokio::sync::watch::Receiver;
 
 use crate::Music;
 
@@ -18,7 +13,7 @@ pub async fn ws_handler(
     ws: WebSocketUpgrade,
     State(state): State<crate::State>,
 ) -> impl IntoResponse {
-    println!("New websocket connection");
+    println!("New WebSocket connection");
     let time_rx = state.0.clone();
     let music_rx = state.1.clone();
     ws.on_upgrade(move |socket| handle_socket(socket, time_rx, music_rx))
@@ -29,70 +24,53 @@ async fn handle_socket(
     mut current_time: Receiver<f64>,
     mut music: Receiver<Option<Music>>,
 ) {
-    let msg_queue: Arc<Mutex<Vec<Message>>> = Arc::new(Mutex::new(vec![]));
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
 
-    let msg_queue_cln = Arc::clone(&msg_queue);
-    let send_task = tokio::spawn(async move {
-        'outer: loop {
-            if let Ok(mut queue) = msg_queue_cln.try_lock() {
-                while let Some(msg) = queue.pop() {
-                    if socket.send(msg).await.is_err() {
-                        break 'outer;
-                    }
-                }
+    let tx_clone = tx.clone();
+    let current_time_task = tokio::spawn(async move {
+        loop {
+            if current_time.changed().await.is_err() {
+                break;
             }
-            std::thread::sleep(Duration::from_millis(100));
+            if let Err(_err) = tx_clone.send(Message::Text(
+                serde_json::json!({
+                    "type": "timechange",
+                    "value": *current_time.borrow()
+                })
+                .to_string()
+                .into(),
+            )) {
+                break;
+            }
         }
     });
 
-    let msg_queue_cln = Arc::clone(&msg_queue);
-    let mut current_time_task = tokio::task::spawn_blocking(move || loop {
-        if Handle::current()
-            .block_on(async { current_time.changed().await })
-            .is_err()
-        {
-            break;
+    let music_change_task = tokio::spawn(async move {
+        loop {
+            if music.changed().await.is_err() {
+                break;
+            }
+            if let Err(_err) = tx.send(Message::Text(
+                serde_json::json!({
+                    "type": "musicchange",
+                    "value": *music.borrow()
+                })
+                .to_string()
+                .into(),
+            )) {
+                break;
+            }
         }
-        msg_queue_cln.blocking_lock().push(Message::Text(
-            serde_json::json!({
-                "type": "timechange",
-                "value": *current_time.borrow()
-            })
-            .to_string()
-            .into(),
-        ));
     });
 
-    let msg_queue_cln = Arc::clone(&msg_queue);
-    let mut music_change_task = tokio::task::spawn_blocking(move || loop {
-        if Handle::current()
-            .block_on(async { music.changed().await })
-            .is_err()
-        {
-            break;
-        }
-        msg_queue_cln.blocking_lock().push(Message::Text(
-            serde_json::json!({
-                "type": "musicchange",
-                "value": *music.borrow()
-            })
-            .to_string()
-            .into(),
-        ));
-    });
-
-    tokio::select! {
-        _ = send_task => {
+    while let Some(msg) = rx.recv().await {
+        if let Err(_err) = socket.send(msg).await {
+            rx.close();
             current_time_task.abort();
             music_change_task.abort();
-        },
-        _ = (&mut current_time_task) => {
-
-        },
-        _ = (&mut music_change_task) => {
-
+            break;
         }
-    };
+    }
 
-    println!("Websocket disconnected.");
+    println!("WebSocket disconnected.");
 }
